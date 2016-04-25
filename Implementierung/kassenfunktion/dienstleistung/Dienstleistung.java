2a916edfb174dbf21bcb4c2ae7e66a33dc5609e5
vs37915 <viola@f-e-sch.de>
2016-04-25 23:49:48 +0200

    added classes

---
+++ Implementierung/kassenfunktion/dienstleistung/Dienstleistung.java
@@ -0,0 +1,44 @@
+package dienstleistung;
+
+public class Dienstleistung {
+
+	private String name;
+	private double preisProLiter;
+	private int verkaufsMenge;
+
+	public Dienstleistung(String name, double preis) {
+		this.setName(name);
+		this.setPreisProLiter(preis);
+		this.setVerkaufsMenge(0);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPreisProLiter() {
+		return preisProLiter;
+	}
+
+	public void setPreisProLiter(double preisProLiter) {
+		this.preisProLiter = preisProLiter;
+	}
+
+	public int getVerkaufsMenge() {
+		return verkaufsMenge;
+	}
+
+	public void setVerkaufsMenge(int verkaufsMenge) {
+		this.verkaufsMenge = verkaufsMenge;
+	}
+
+	public static Dienstleistung[] listeDienstleistungen = {
+			new Dienstleistung("Literzahl - heiß in Beutel", 0.1),
+			new Dienstleistung("Literzahl - heiß in Flaschen", 0.2),
+			new Dienstleistung("Literzahl - kalt", 0.3) };
+
+}
---
+++ Implementierung/kassenfunktion/produkte/Abfüllmaterial.java
@@ -0,0 +1,10 @@
+package produkte;
+
+public class Abfüllmaterial extends Produkt {
+
+	public Abfüllmaterial(String name, double preis, int menge, int untergrenze) {
+		super(name, preis, menge, untergrenze);
+
+	}
+
+}
