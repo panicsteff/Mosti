7f0e33b65d7184e5b5a28556e72c8ebe2a986db0
vs37915 <viola@f-e-sch.de>
2016-05-27 22:35:55 +0200

    mini überarbeitung

--- Mavenproject/MostiSoftware/src/main/java/main/Main.java
+++ Mavenproject/MostiSoftware/src/main/java/main/Main.java
@@ -1,10 +1,5 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.Date;
-
-import logik.verkaufsverwaltung.Verkaufsposition;
-import persistenz.VerkäufeDB;
 import account.M_Startseite;
 
 public class Main {
@@ -12,15 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 	
 		M_Startseite startseite = new M_Startseite();
-		
-//		VerkäufeDB v = new VerkäufeDB();
-//		java.util.Date datum = new Date();
-//		java.sql.Date d = new java.sql.Date(datum.getTime());
-//		System.out.println(d);
-//		ArrayList<Verkaufsposition> einkaufsliste = v.tagesVerkäufeLaden(d);
-//		for(Verkaufsposition vp: einkaufsliste){
-//			System.out.println(vp.getName()+ vp.getPreis()+ vp.getLiterzahl()+ vp.getVerkaufsMenge());
-//		}
 		
 	}
 
