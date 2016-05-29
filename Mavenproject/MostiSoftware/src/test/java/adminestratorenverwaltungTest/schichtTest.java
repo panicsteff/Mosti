55bcbdec9ce68dbd17b50f04051b6885fdd9b891
panicsteff <steffi_lonehart@gmx.de>
2016-05-29 11:17:03 +0200

    anfang mit junit

---
+++ Mavenproject/MostiSoftware/src/test/java/adminestratorenverwaltungTest/schichtTest.java
@@ -0,0 +1,21 @@
+package adminestratorenverwaltungTest;
+
+
+import java.util.Date;
+import org.junit.Test;
+
+import administratorverwaltung.Schicht;
+
+public class schichtTest {
+	
+	 @Test
+	 public void sollSchichtErstellenPlusRueckgabe(){
+		 
+		 private Date uhrzeit;
+		 int mitarbeiterID = 1;
+		 Schicht schicht = new Schicht();
+		 
+		 
+	 }
+
+}
